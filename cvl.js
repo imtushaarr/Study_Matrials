7d85eafced485ef1001b328fef2f6afc9cdebf8a
Tushar Gupta <tusharguptagps@gmail.com>
2024-06-06 12:22:35 +0000

    Length Parameter

--- cvl.js
+++ cvl.js
@@ -1,4 +1,4 @@
-const myName = "Tushar"
+/*const myName = "Tushar"
 let myEmail = "tushar@krafcool.com"
 var myPassword = "tushar123"
 myCity = "Kharar"
@@ -8,4 +8,13 @@ myEmail = "agustya@krafcool.com"
 myPassword = "agustya123"
 myCity = "Rewa"
 
-console.log([myName, myEmail, myPassword, myCity]);
+console.log([myName, myEmail, myPassword, myCity]);*/
+
+let myName = function (){
+    let nameLen = "tushar"
+    nameLen = "agustya"
+    return nameLen.length;
+}
+
+console.log(myName())
+
