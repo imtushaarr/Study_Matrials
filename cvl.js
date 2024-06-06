e660cf9f7f28bae9dafdb7e24b95caf081e237bf
Tushar Gupta <tusharguptagps@gmail.com>
2024-06-06 12:08:42 +0000

    Uses of Vae, Let, Const

---
+++ cvl.js
@@ -0,0 +1,11 @@
+const myName = "Tushar"
+let myEmail = "tushar@krafcool.com"
+var myPassword = "tushar123"
+myCity = "Kharar"
+
+// myName = "Agustya"
+myEmail = "agustya@krafcool.com"
+myPassword = "agustya123"
+myCity = "Rewa"
+
+console.log([myName, myEmail, myPassword, myCity]);
